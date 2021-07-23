613880b5217438dc370280132ad95e78c19f0c53
danielvtfs <danielvtfs@gmail.com>
2021-07-23 13:28:38 -0300

    primeiro comit

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 const express = require('express');
 const mongoose = require('mongoose');
-var cors = require('cors')
-
+var cors = require('cors');
 
 const ToDoSchema = require('./schemas/ToDoSchema');
 
 const server = express();
 
-server.use(cors())
+server.use(cors());
 
 server.use(express.json());
 
-mongoose.connect('mongodb+srv://........mongodb.net/todo_list?retryWrites=true&w=majority',
+mongoose.connect(
+  'mongodb+srv://danielvtfs:d2601203@clusterdaniel.awg5x.mongodb.net/task_list?retryWrites=true&w=majority',
   {
     useUnifiedTopology: true,
-    useNewUrlParser: true
+    useNewUrlParser: true,
   }
 );
 
 server.get('/', (req, res) => {
   return res.json({ message: 'Seja bem vindo à API do TODO_LIST' });
-})
+});
 
 server.post('/todo', async (req, res) => {
   const { title, date } = req.body;
   if (!title || !date) {
-    return res.status(400).json({ message: 'Validations Fails' })
+    return res.status(400).json({ message: 'Validations Fails' });
   }
-  const todo = await ToDoSchema.create(req.body)
+  const todo = await ToDoSchema.create(req.body);
   return res.status(201).json(todo);
 });
 
@@ -44,15 +44,16 @@ server.get('/todo/:id', async (req, res) => {
 
 server.put('/todo/:id', async (req, res) => {
   const { id } = req.params;
-  const todo = await ToDoSchema.findOneAndUpdate({ '_id': id }, req.body);
+  const todo = await ToDoSchema.findOneAndUpdate({ _id: id }, req.body);
   return res.json(todo);
-})
+});
 
 server.delete('/todo/:id', async (req, res) => {
   const { id } = req.params;
-  const todo = await ToDoSchema.deleteOne({ '_id': id });
+  const todo = await ToDoSchema.deleteOne({ _id: id });
   return res.json({ message: 'Successfully deleted' });
-})
+});
 
-
-server.listen(3333, () => console.log('Servidor iniciado em http://localhost:3333'))
+server.listen(3333, () =>
+  console.log('Servidor iniciado em http://localhost:3333')
+);
